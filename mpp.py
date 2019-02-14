062d868ef7688bd4b6138dcf5bb2dfe601716563
Feiyi Wang <fwang2@gmail.com>
2019-02-14 11:48:41 -0500

    did init

--- mpp.py
+++ mpp.py
@@ -2,7 +2,10 @@ import numpy as np
 import argparse
 import sys
 
-args = None
+
+# module level globals
+means, covs = {}, {}
+covavg, varavg = None, None
 
 # def check_py_version():
 #     if sys.version_info <= (3, 5):
@@ -11,58 +14,80 @@ args = None
 #         sys.exit(1)
 
 
-def parse():
+def parse_cmdline():
     """ parse command line """
-    global args
     parser = argparse.ArgumentParser(description=" MPP Demo ")
     parser.add_argument("train", metavar="Training", help="training data file")
     parser.add_argument("test", metavar="Testing", help="test data file")
     parser.add_argument("case", type=int, choices=range(1,4), help="choose from 1, 2, 3")
-    args = parser.parse_args()
+    return parser.parse_args()
+ 
+def mpp1(Tr, sample, classn, case, pw):
+    """ return a label """
+    assert varavg != None, \
+        "mpp is not initialized yet, call load_data(...), then mpp_init(...) first."
 
-def pre_calc(df):
-    pass
+def mpp_init(Tr, Te, classes):
+    """ initialize """
+    global covs, means, covavg, varavg
 
-def mpp(df):
-    """
-    df['train_data'] - training data
-    df['train_label'] - training label
-    df['test_data'] - testing data
-    df['test_label'] - testing label
-    df['case'] - chodef mpp(df):sen case
-    """
-    pass
+    covsum = None
 
+    for c in classes:
+        arr = Tr[Tr[:, -1] == c]
+        covs[c] = np.cov(arr[:,:-1]) # do cov on array sans last column
+        means[c] = np.mean(arr[:,:-1], axis=0) # mean along rows
+        if covsum is None:
+            covsum = covs[c]
+        else:
+            covsum += covs[c]
+
+    # used by case II
+    covavg = covsum / len(classes)
+
+    # used by case I
+    varavg = np.sum(covavg) / len(classes)
+
+    return covs, means, covavg, varavg
+    
 def load_data(trainf='datasets/synth.tr', testf='datasets/synth.te'):
+    """ initialize with training and testing data, return a tuple of
+    (training, train_label, test, test_label, etc.)
+    """
+    global covs, means, covavg, varavg 
 
     # process training data
-    data = np.loadtxt(trainf)
-    train_data = data[:,:-1]                    # return all feature columns except last
-    train_label = data[:, -1].astype(int)       # put all labels in separate array
-    
+    train_data = np.genfromtxt(trainf)
+    # return all feature columns except last
+    # train_data = train_data[:,:-1]    
+
     # number of unique class
-    classes = np.unique(train_label)
+    classes = np.unique(train_data[:,-1])
 
     # number of rows and columns
     nr, nc = np.shape(train_data)
+    nf = nc - 1
 
     # processing testing data
-    data = np.loadtxt(testf)
-    test_data = data[:,:-1]
-    test_label = data[:, -1].astype(int)
+    test_data = np.genfromtxt(testf)
 
     # sanity check (# of columns must be equal)
     assert np.size(test_data, 1) == np.size(train_data, 1)
 
+    # init prio probability, equal distribution
+    classn = len(classes)
+    pw = np.full(classn, 1/classn)
 
-    return (train_data, train_label, test_data, test_label, nr, nc, classes)
+    return (train_data, test_data, classes, nr, nf, pw)
 
 
 def main():
 
     # now parse command line
-    parse()
-
+    args = parse_cmdline()
+    train_data, test_data, classes, nr, nf, pw = load_data(args.train, args.test)
+    mpp_init(train_data, test_data, classes)
+    mpp1(train_data, None, None, None, pw)
 
     # print("Loaded: {} records of training data, {} feature columns, {} classes".format(nr, nc, classes))
     # print("Loaded: {} records of test data, sanity check okay".format(np.size(test_data)))
