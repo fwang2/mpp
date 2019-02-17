34b66a1260c95a14a7163e409a9122231e38bc03
Feiyi Wang <fwang2@gmail.com>
2019-02-17 12:40:56 -0500

    use sklearn

--- mpp.py
+++ mpp.py
@@ -2,8 +2,6 @@ import numpy as np
 import argparse
 import sys
 
-
-
 # def check_py_version():
 #     if sys.version_info <= (3, 5):
 #         print("You are running Python version below 3.5")
@@ -16,80 +14,77 @@ def parse_cmdline():
     parser = argparse.ArgumentParser(description=" MPP Demo ")
     parser.add_argument("train", metavar="Training", help="training data file")
     parser.add_argument("test", metavar="Testing", help="test data file")
-    parser.add_argument("case", type=int, choices=range(1,4), help="choose from 1, 2, 3")
+    parser.add_argument(
+        "case", type=int, choices=range(1, 4), help="choose from 1, 2, 3")
     return parser.parse_args()
 
-class MPP:
 
-    def __init__(self, trainf, testf):
-        # process training data
-        self.train_data = np.genfromtxt(trainf)
-        # return all feature columns except last
-        # train_data = train_data[:,:-1]    
-
-        # number of unique class
-        self.classes = np.unique(self.train_data[:,-1])
-
-        # number of rows and columns
-        self.train_nr, self.nc = np.shape(self.train_data)
-        self.nf = self.nc - 1
-
-        # processing testing data
-        self.test_data = np.genfromtxt(testf)
-        self.test_nr, _ = np.shape(self.test_data)
-
-        # sanity check (# of columns must be equal)
-        assert np.size(self.test_data, 1) == np.size(self.train_data, 1)
-
+class mpp:
+    def __init__(self, case=1):
         # init prio probability, equal distribution
-        self.classn = len(self.classes)
-        self.pw = np.full(self.classn, 1/self.classn)
+        # self.classn = len(self.classes)
+        # self.pw = np.full(self.classn, 1/self.classn)
 
-        self.covs, self.means, self.covavg, self.varavg = \
-            self.train(self.train_data, self.classes)
+        # self.covs, self.means, self.covavg, self.varavg = \
+        #     self.train(self.train_data, self.classes)
+        self.case_ = case
+        
 
-    def train(self, Tr, classes):
-        """ initialize: return (covs, means, covavg, varavg) """
-        covs, means = {}, {}
-        covavg, varavg, covsum = None, None, None
+    def fit(self, Tr, y):
+        self.covs_, self.means_ = {}, {}
+        self.covavg_, self.varavg_, self.covsum_ = None, None, None
 
-        for c in classes:
+        self.classes_ = np.unique(y)
+        self.classn_ = len(self.classes_)
+        self.pw_ = np.full(self.classn_, 1/self.classn_)
+        
+        for c in self.classes_:
             arr = Tr[Tr[:, -1] == c]
-            covs[c] = np.cov(arr[:,:-1]) # do cov on array sans last column
-            means[c] = np.mean(arr[:,:-1], axis=0) # mean along rows
-            if covsum is None:
-                covsum = covs[c]
+            self.covs_[c] = np.cov(arr[:, :-1])  # do cov on array sans last column
+            self.means_[c] = np.mean(arr[:, :-1], axis=0)  # mean along rows
+            if self.covsum_ is None:
+                self.covsum_ = self.covs_[c]
             else:
-                covsum += covs[c]
+                self.covsum_ += self.covs_[c]
 
         # used by case II
-        covavg = covsum / len(classes)
+        self.covavg_ = self.covsum_ / len(self.classes_)
 
         # used by case I
-        varavg = np.sum(covavg) / len(classes)
+        self.varavg_ = np.sum(self.covavg_) / len(self.classes_)
 
-        return covs, means, covavg, varavg
 
-    def set_case(self, case):
-        self.case = case
-
-    def evaluate(self, case = 1):
+    def predict(self, T):
         """ eval all data """
-        disc = np.zeros(len(self.classes))
-        for i in range(self.test_nr):
-            if case == 1:
-                for c in self.classes:
-                    c = int(c)  # class is float, convert to int for indexing
-                    ti = self.test_data[i,:-1]  # get ith row
-                    edist = np.linalg.norm(self.means[c] - ti)
-                    disc[c] = -edist*edist/(2*self.varavg) + np.log(self.pw[c])  
+        disc = np.zeros(self.classn_)
+        nr, _ = T.shape
+        for i in range(nr):
+            if self.case_ == 1:
+                for c in self.classes_:
+                    ti = T[i, :-1]  # get ith row
+                    edist = np.linalg.norm(self.means_[c] - ti)
+                    disc[c] = -(edist * edist) / (2 * self.varavg_) + np.log(self.pw_[c])
                     print("edist: {}, disc{}".format(edist, disc[c]))
-            elif case == 2:
+            elif self.case_ == 2:
                 pass
-            elif case == 3:
+            elif self.case_ == 3:
                 pass
             else:
-                raise Exception("Can't handle case value", case)
+                raise Exception("Can't handle case value", self.case_)
+
+
+def load_data(f="datasets/synth.tr"):
+    """ Assume data format:
+    feature1 feature 2 ... label 
+    """
+
+    # process training data
+    data = np.genfromtxt(f)
+    # return all feature columns except last
+    X = data[:,:-1]
+    y = data[:, -1].astype(int)
+ 
+    return X, y
 
 def main():
 
@@ -98,8 +93,10 @@ def main():
     # mpp = MPP(args.train, args.test)
     # mpp.train(args.case)
 
-    m = MPP("datasets/synth.tr", "datasets/synth.te")
-    m.eval(case = 1)
+    # m = MPP("datasets/synth.tr", "datasets/synth.te")
+    # m.eval(case=1)
+
+    pass
 
 if __name__ == "__main__":
     main()
