07c0e5c050ca9ae57ac1d85325a6d8f7009b9058
Feiyi Wang <fwang2@gmail.com>
2019-02-17 16:34:31 -0500

    fix

--- mpp.py
+++ mpp.py
@@ -28,20 +28,20 @@ class mpp:
         # self.covs, self.means, self.covavg, self.varavg = \
         #     self.train(self.train_data, self.classes)
         self.case_ = case
-        
 
     def fit(self, Tr, y):
         self.covs_, self.means_ = {}, {}
-        self.covavg_, self.varavg_, self.covsum_ = None, None, None
+        self.covsum_ = None
 
         self.classes_ = np.unique(y)
         self.classn_ = len(self.classes_)
-        self.pw_ = np.full(self.classn_, 1/self.classn_)
-        
+        self.pw_ = np.full(self.classn_, 1 / self.classn_)
+
         for c in self.classes_:
-            arr = Tr[Tr[:, -1] == c]
-            self.covs_[c] = np.cov(arr[:, :-1])  # do cov on array sans last column
-            self.means_[c] = np.mean(arr[:, :-1], axis=0)  # mean along rows
+            arr = Tr[y == c]
+            print(arr.shape)
+            self.covs_[c] = np.cov(arr)
+            self.means_[c] = np.mean(arr, axis=0)  # mean along rows
             if self.covsum_ is None:
                 self.covsum_ = self.covs_[c]
             else:
@@ -53,26 +53,21 @@ class mpp:
         # used by case I
         self.varavg_ = np.sum(self.covavg_) / len(self.classes_)
 
-
     def predict(self, T):
         """ eval all data """
+        y = []
         disc = np.zeros(self.classn_)
         nr, _ = T.shape
         for i in range(nr):
-            if self.case_ == 1:
-                for c in self.classes_:
-                    ti = T[i, :-1]  # get ith row
-                    edist = np.linalg.norm(self.means_[c] - ti)
-                    disc[c] = -(edist * edist) / (2 * self.varavg_) + np.log(self.pw_[c])
-                    print("edist: {}, disc{}".format(edist, disc[c]))
-            elif self.case_ == 2:
-                pass
-            elif self.case_ == 3:
-                pass
-            else:
-                raise Exception("Can't handle case value", self.case_)
+            for c in self.classes_:
+                ti = T[i, :-1]  # get ith row
+                edist = np.linalg.norm(self.means_[c] - ti)
+                disc[c] = -(edist * edist) / (2 * self.varavg_) + np.log(
+                    self.pw_[c])
+            y.append(disc.argmax())
 
-
+        return y
+        
 def load_data(f="datasets/synth.tr"):
     """ Assume data format:
     feature1 feature 2 ... label 
@@ -81,22 +76,23 @@ def load_data(f="datasets/synth.tr"):
     # process training data
     data = np.genfromtxt(f)
     # return all feature columns except last
-    X = data[:,:-1]
+    X = data[:, :-1]
     y = data[:, -1].astype(int)
- 
+
     return X, y
+
 
 def main():
 
-    # now parse command line
-    # args = parse_cmdline()
-    # mpp = MPP(args.train, args.test)
-    # mpp.train(args.case)
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.metrics import accuracy_score
 
-    # m = MPP("datasets/synth.tr", "datasets/synth.te")
-    # m.eval(case=1)
-
-    pass
+    Xtrain, ytrain = load_data("datasets/synth.tr")
+    Xtest, ytest = load_data("datasets/synth.te")
+    model = mpp()
+    model.fit(Xtrain, ytrain)
+    y_model = model.predict(Xtest)
+    accuracy_score(ytest, y_model)
 
 if __name__ == "__main__":
     main()
